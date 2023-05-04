8eacd504d346379c13a7c1fce0389bfb23473853
monimunozalzate <106264010+monimunozalzate@users.noreply.github.com>
2023-05-03 20:32:58 -0500

    hover cards con usewindowsize

--- src/components/custom/cardComponent/CardComponent.jsx
+++ src/components/custom/cardComponent/CardComponent.jsx
@@ -1,59 +1,89 @@
 import { Box, Grid, Typography } from "@mui/material";
 import { useState } from "react";
 import styles from "../card/Card.module.css";
+import { useWindowSize } from "../../../utils/useWindowSize";
 
 const CardComponent = ({ project }) => {
   const [enter, setEnter] = useState(false);
-  //   console.log(enter);
+  const size = useWindowSize();
+
   return (
     <>
-      {enter ? (
+      {size.width > 900 ? (
         <Grid
           item
           lg={6}
-          sm={12}
-          key={project.id}
-          className={styles.card}
-          onMouseEnter={() => setEnter(true)}
-          onMouseLeave={() => setEnter(false)}
-        >
-          <img
-            src={project.source}
-            alt={project.name}
-            className={styles.opacImg}
-          />
-          <Box className={styles.linksBox}>
-            <Typography variant="body1" className={styles.link}>
-              VIEW PROJECT
-            </Typography>
-            <Typography variant="body1" className={styles.link}>
-              VIEW CODE
-            </Typography>
-          </Box>
-          <Typography variant="h2" className={styles.name}>
-            {project.name}
-          </Typography>
-          <Typography variant="body1" className={styles.language}>
-            {project.languages}
-          </Typography>
-        </Grid>
-      ) : (
-        <Grid
-          item
-          lg={6}
-          sm={12}
+          xs={12}
           key={project.id}
           className={styles.card}
           onMouseEnter={() => setEnter(true)}
           onMouseLeave={() => setEnter(false)}
         >
           <img src={project.source} alt={project.name} className={styles.img} />
+          {enter && (
+            <Box className={styles.linksBox}>
+              <a
+                rel="noopener noreferrer"
+                href="https://www.frontendmentor.io/challenges/singlepage-developer-portfolio-bBVj2ZPi-x/hub"
+                target="_blank"
+                className={styles.link}
+              >
+                VIEW PROJECT
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="https://www.frontendmentor.io/challenges/singlepage-developer-portfolio-bBVj2ZPi-x/hub"
+                target="_blank"
+                className={styles.link}
+              >
+                VIEW CODE
+              </a>
+            </Box>
+          )}
+
           <Typography variant="h2" className={styles.name}>
             {project.name}
           </Typography>
           <Typography variant="body1" className={styles.language}>
-            {project.languages}
+            {project.languages.join(" ")}
           </Typography>
+        </Grid>
+      ) : (
+        <Grid
+          item
+          lg={6}
+          xs={12}
+          key={project.id}
+          className={styles.card}
+          onMouseEnter={() => setEnter(true)}
+          onMouseLeave={() => setEnter(false)}
+        >
+          <img src={project.source} alt={project.name} className={styles.img} />
+
+          <Typography variant="h2" className={styles.name}>
+            {project.name}
+          </Typography>
+          <Typography variant="body1" className={styles.language}>
+            {project.languages.join(" ")}
+          </Typography>
+          <Box className={styles.linksBox}>
+            <a
+              rel="noopener noreferrer"
+              href="https://www.frontendmentor.io/challenges/singlepage-developer-portfolio-bBVj2ZPi-x/hub"
+              target="_blank"
+              className={styles.link}
+            >
+              VIEW PROJECT
+            </a>
+            <a
+              rel="noopener noreferrer"
+              href="https://www.frontendmentor.io/challenges/singlepage-developer-portfolio-bBVj2ZPi-x/hub"
+              target="_blank"
+              className={styles.link}
+            >
+              VIEW CODE
+            </a>
+          </Box>
         </Grid>
       )}
     </>
