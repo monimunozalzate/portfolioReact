57aec4c47d765e220eff8de6efb34ca6057b1fda
monimunozalzate <106264010+monimunozalzate@users.noreply.github.com>
2023-05-05 21:49:31 -0500

    projects urls

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,28 +1,38 @@
 export const projects = [
-    { id: 1, source: "src/assets/images/thumbnail-project-1-large.webp", name: "DESIGN PORTFOLIO", languages: ["HTML", "CSS"] },
-    {
-        id: 2,
-        source: "src/assets/images/thumbnail-project-2-large.webp",
-        name: "E-LEARNING LANDING PAGE",
-        languages: ["HTML", "CSS"],
-    },
-    {
-        id: 3,
-        source: "src/assets/images/thumbnail-project-3-large.webp",
-        name: "TODO WEB APP",
-        languages: ["HTML", "CSS", "JAVASCRIPT"],
-    },
-    {
-        id: 4,
-        source: "src/assets/images/thumbnail-project-4-large.webp",
-        name: "ENTERTAINMENT WEB APP",
-        languages: ["HTML", "CSS", "JAVASCRIPT"],
-    },
-    { id: 5, source: "src/assets/images/thumbnail-project-5-large.webp", name: "GAME", languages: ["HTML", "CSS", "JAVASCRIPT"] },
-    {
-        id: 6,
-        source: "src/assets/images/thumbnail-project-6-large.webp",
-        name: "GALLERY",
-        languages: ["HTML", "CSS", "JAVASCRIPT"],
-    },
+  {
+    id: 1,
+    source: "https://res.cloudinary.com/dwqrlr45w/image/upload/v1683341047/react-portfolio-frontendMentor/images/thumbnail-project-1-large_wgsb55.webp",
+    name: "DESIGN PORTFOLIO",
+    languages: ["HTML", "CSS"],
+  },
+  {
+    id: 2,
+    source: "https://res.cloudinary.com/dwqrlr45w/image/upload/v1683341048/react-portfolio-frontendMentor/images/thumbnail-project-2-large_kt7qvr.webp",
+    name: "E-LEARNING LANDING PAGE",
+    languages: ["HTML", "CSS"],
+  },
+  {
+    id: 3,
+    source: "https://res.cloudinary.com/dwqrlr45w/image/upload/v1683341048/react-portfolio-frontendMentor/images/thumbnail-project-3-large_ubg1yv.webp",
+    name: "TODO WEB APP",
+    languages: ["HTML", "CSS", "JAVASCRIPT"],
+  },
+  {
+    id: 4,
+    source: "https://res.cloudinary.com/dwqrlr45w/image/upload/v1683341049/react-portfolio-frontendMentor/images/thumbnail-project-4-large_r4mzxh.webp",
+    name: "ENTERTAINMENT WEB APP",
+    languages: ["HTML", "CSS", "JAVASCRIPT"],
+  },
+  {
+    id: 5,
+    source: "https://res.cloudinary.com/dwqrlr45w/image/upload/v1683341049/react-portfolio-frontendMentor/images/thumbnail-project-5-large_mleygz.webp",
+    name: "GAME",
+    languages: ["HTML", "CSS", "JAVASCRIPT"],
+  },
+  {
+    id: 6,
+    source: "https://res.cloudinary.com/dwqrlr45w/image/upload/v1683341050/react-portfolio-frontendMentor/images/thumbnail-project-6-large_y8lftb.webp",
+    name: "GALLERY",
+    languages: ["HTML", "CSS", "JAVASCRIPT"],
+  },
 ];
